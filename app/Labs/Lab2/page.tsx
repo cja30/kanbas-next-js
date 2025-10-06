8880355efdd0429ac9c5403d8fdb0a8ed969ef2f
Charlotte Anderson <charlottejandersonmv@gmail.com>
2025-10-05 23:23:00 -0700

    A2

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -27,7 +27,7 @@ export default function Lab2() {
       <h3>Styling with the STYLE attribute</h3>
       <p>
         Style attribute allows configuring look and feel
-        right on the element. Although it's very convenient
+        right on the element. Although it&apos;s very convenient
         it is considered bad practice and you should avoid
         using the style attribute
       </p>
@@ -35,11 +35,11 @@ export default function Lab2() {
       <div id="wd-css-id-selectors">
         <h3>ID selectors</h3>
         <p id="wd-id-selector-1">
-          Instead of changing the look and feel of all the 
+          Instead of changing the look and feel of all the
           elements of the same name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and
+          Here&apos;s another paragraph using a different ID and a different look and
           feel
         </p>
       </div>
@@ -48,7 +48,7 @@ export default function Lab2() {
         <h3>Class selectors</h3>
 
         <p className="wd-class-selector">
-          Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+          Instead of using IDs to refer to elements, you can use an element&apos;s CLASS attribute
         </p>
 
         <h4 className="wd-class-selector">
@@ -63,14 +63,14 @@ export default function Lab2() {
             Selectors can be combined to refer elements in particular
             places in the document
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as
+              This paragraph&apos;s red background is referenced as
               <br />
               .selector-2 .selector3<br />
               meaning the descendant of some ancestor.<br />
               <span className="wd-selector-4">
                 Whereas this span is a direct child of its parent
               </span><br />
-              You can combine these relationships to create specific 
+              You can combine these relationships to create specific
               styles depending on the document structure
             </p>
           </div>
