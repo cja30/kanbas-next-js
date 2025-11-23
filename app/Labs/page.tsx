ec35442a342b292e43253b59eaa8973ee8a18781
Charlotte Anderson <charlottejandersonmv@gmail.com>
2025-11-23 00:57:05 -0500

    fix

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
-import store from "../state/store"; 
-import { Provider } from "react-redux";
 
 export default function LabsIndex() {
   redirect("/Labs/Lab1");
+  return null;
 }
