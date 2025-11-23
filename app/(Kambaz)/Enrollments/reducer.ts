ddea15e9d0b372629dd70fde0b7c831edc20ac9f
Charlotte Anderson <charlottejandersonmv@gmail.com>
2025-11-23 00:45:55 -0500

    fix

--- app/(Kambaz)/Enrollments/reducer.ts
+++ app/(Kambaz)/Enrollments/reducer.ts
@@ -1,13 +1,15 @@
 "use client";
 import { createSlice } from "@reduxjs/toolkit";
 import * as db from "../Database";
+import { v4 as uuidv4 } from "uuid";
 
 const enrollmentsSlice = createSlice({
   name: "enrollments",
   initialState: {
-    enrollments: db.enrollments,
+    enrollments: [...db.enrollments],
     showAll: false,
   },
+
   reducers: {
     toggleShowAll: (state) => {
       state.showAll = !state.showAll;
@@ -15,13 +17,23 @@ const enrollmentsSlice = createSlice({
 
     enroll: (state, { payload }) => {
       const { user, course } = payload;
-      if (!state.enrollments.some((e) => e.user === user && e.course === course)) {
-        state.enrollments.push({ user, course });
+
+      const exists = state.enrollments.some(
+        (e) => e.user === user && e.course === course
+      );
+
+      if (!exists) {
+        state.enrollments.push({
+          _id: uuidv4(),   
+          user,
+          course,
+        });
       }
     },
 
     unenroll: (state, { payload }) => {
       const { user, course } = payload;
+
       state.enrollments = state.enrollments.filter(
         (e) => !(e.user === user && e.course === course)
       );
