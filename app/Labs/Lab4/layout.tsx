dc61dd5f729aefc5db628e811f527523f9d05e55
Charlotte Anderson <charlottejandersonmv@gmail.com>
2025-11-23 00:49:28 -0500

    fix

--- app/Labs/Lab4/layout.tsx
+++ app/Labs/Lab4/layout.tsx
@@ -2,7 +2,12 @@
 
 import { Provider } from "react-redux";
 import store from "../state";
+import React from "react";
 
-export default function Lab4Layout({ children }) {
+interface Lab4LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Lab4Layout({ children }: Lab4LayoutProps) {
   return <Provider store={store}>{children}</Provider>;
 }
