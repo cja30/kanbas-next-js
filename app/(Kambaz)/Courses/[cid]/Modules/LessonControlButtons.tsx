986db819cb12fe2a983b44d9b17341321dfd6a4a
Charlotte Anderson <charlottejandersonmv@gmail.com>
2025-11-23 00:29:02 -0500

    fix?

--- app/(Kambaz)/Courses/[cid]/Modules/LessonControlButtons.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/LessonControlButtons.tsx
@@ -1,7 +1,19 @@
+"use client";
+
 import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 
-export default function LessonControlButtons() {
+type LessonControlButtonsProps = {
+  modules?: any;
+  module?: any;
+  lesson?: any;
+};
+
+export default function LessonControlButtons({
+  modules,
+  module,
+  lesson,
+}: LessonControlButtonsProps) {
   return (
     <div className="float-end">
       <GreenCheckmark />
