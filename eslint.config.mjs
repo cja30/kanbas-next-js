505048fe9f3ae53ff49fbb2431c855550513a8b1
Charlotte Anderson <charlottejandersonmv@gmail.com>
2025-11-23 00:23:21 -0500

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
+export default [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/rules-of-hooks": "off",
-      "@next/next/no-async-client-component": "off", 
-    },
+    ignores: ["**/*"],
+    rules: {},
   },
 ];
-
-export default eslintConfig;
